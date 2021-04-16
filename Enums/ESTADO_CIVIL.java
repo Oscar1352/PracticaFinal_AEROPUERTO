5d510a93a590fd4edc61eed172e634304ba0479e
Oscar Luna <carolina.151270@gmail.com>
2021-04-16 00:55:32 -0600

    Mi primer commit

---
+++ Enums/ESTADO_CIVIL.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Oscar Luna
+ */
+public enum ESTADO_CIVIL {
+    //Establezco el estado civil del usuario
+    SOLTERO,CASADO,DIVORCIADO;
+    
+}
